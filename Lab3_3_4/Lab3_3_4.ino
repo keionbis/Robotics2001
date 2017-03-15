a1e228ba4b9ced793a3f1a4588133ef839dec9d5
keionbis <keion96@gmail.com>
2017-03-15 15:54:48 -0400

    Updated

--- Lab3_3_4/Lab3_3_4.ino
+++ Lab3_3_4/Lab3_3_4.ino
@@ -1,28 +1,26 @@
-#define ledPin 13
+#define output 13
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
-  
-  // initialize timer1 
-  noInterrupts();           // disable all interrupts
+  pinMode(output, OUTPUT); 
+  noInterrupts();          
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1  = 0;
 
-  OCR1A = 31250;            // compare match register 16MHz/256/2Hz
-  TCCR1B |= (1 << WGM12);   // CTC mode
-  TCCR1B |= (1 << CS12);    // 256 prescaler 
-  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
-  interrupts();             // enable all interrupts
+  OCR1A = 31250;            
+  TCCR1B |= (1 << WGM12);   
+  TCCR1B |= (1 << CS12);    
+  TIMSK1 |= (1 << OCIE1A);  
+  interrupts();             
 }
 
-ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
+ISR(TIMER1_COMPA_vect)          
 {
-  digitalWrite(ledPin, digitalRead(ledPin) ^ 1);   // toggle LED pin
-}
+  digitalWrite(output, digitalRead(output) ^ 1);   
+  }
 
 void loop()
 {
-  // your program here...
+   
 }
